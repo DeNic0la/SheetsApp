174f3883bb4189dc8377bbc9493b002db25d88e2
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2022-12-10 23:05:06 +0100

    init

--- Code.js
+++ Code.js
@@ -1,3 +1,12 @@
-function myFunction() {
-  
+function onOpen() {
+    let ui = SpreadsheetApp.getUi();
+    ui.createAddonMenu().addItem("testFunction", "myTest")
+}
+
+function myTest() {
+    console.log("Hello this is a test");
+    SpreadsheetApp.getUi().alert("Test");
+}
+function onInstall(){
+    onOpen();
 }
