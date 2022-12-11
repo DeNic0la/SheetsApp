b465eccd26510e0c72e6c809ddbb1c2e3bca8c38
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2022-12-11 09:48:31 +0100

    add Helper

--- Code.js
+++ Code.js
@@ -1,3 +1,14 @@
+
+
+const AREA_NAME_NOON = "nachmittage"
+const AREA_NAME_MEETINGS= "sitzungen"
+
+const NOON_END_TIME = [17,15]
+
+
+
+
+
 function onOpen() {
     let ui = SpreadsheetApp.getUi();
     ui.createAddonMenu().addItem("testFunction", "myTest")
@@ -10,4 +21,33 @@ function myTest() {
 }
 function onInstall(){
     onOpen();
+}
+
+function getDataByRange(rangeName) {
+    return SpreadsheetApp.getActiveSpreadsheet().getRangeByName(rangeName).getValues();
+}
+function noonArrayToObject(array){
+    let date = array[0]
+    let timePlace = array[1].split("/") // 00:00 / Place to ["00:00", "Place"]
+    let time = timePlace[0].trim().split(":") // " 00:00 " to ["00","00"]
+    let startDate = new Date(date);
+    let endDate = new Date(date);
+
+    startDate.setHours(parseInt(time[0]));
+    startDate.setMinutes(parseInt(time[1]));
+
+    endDate.setHours(NOON_END_TIME[0]);
+    endDate.setMinutes(NOON_END_TIME[1]);
+
+    return  {
+        startDate,
+        endDate,
+        place: timePlace[1],
+        name: array[2],
+        lead: array[3],
+        lunch: array[4],
+        excused: array[5],
+        impMessage: array[6],
+        calId: array[7]
+    }
 }
